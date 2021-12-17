aec17cafe9a9345accce04ba525cd27fbfacaae7
jaredhall18 <jaredhall18@gmail.com>
2021-12-17 12:10:04 -0700

    initial test setup

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,32 @@
-// Write your tests here
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const server = require("../api/server");
+const User = require("./model/model");
+
+const user1 = {
+  id: 1,
+  username: "Jaredtester",
+  password: "1234",
+};
+const user2 = {
+  id: 2,
+  username: "Jaredtester2",
+  password: "1234",
+};
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("users").truncate();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
 test('sanity', () => {
   expect(true).toBe(false)
 })
